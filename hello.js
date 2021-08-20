ad9c3a68edeee164f6d4e51aa7bc0f80f37dcf3a
rizkyputra <rizkyagrasyahputra19@gmail.com>
2021-08-20 14:34:55 +0700

    first commit

---
+++ hello.js
@@ -0,0 +1,50 @@
+// var teks = "Rizky"
+// var nilai = `Halo ${teks}`
+
+// console.log(nilai)
+
+// const hayo = (a) => {
+//     console.log(`Hello ${a}`)
+//     nilai(6)
+// }
+
+// const nilai = (b) => {
+//     console.log(`rizky ${b}`)
+// }
+
+// hayo(5)
+
+// const b = [
+//     {nama: "rizky", kelas: "backend"},
+//     {nama: "rayhan", kelas: "frontend"},
+// ];
+
+// // console.log(b[0]["nama"]);
+
+// console.log(b[3].nama)
+
+// let animal = "gajah";
+// const condition = true;
+    
+// if(condition == 1){
+// let animal = "kelelawar";
+// };    
+
+// console.log(animal);
+
+// const pass = "12345"
+
+// function cekPassword (password) {
+//     if (password == pass) {
+//         console.log("Password Benar")
+//     } else {console.log("Password Salah")}
+// };
+
+// cekPassword(12345);
+
+const Password = (pass) => {
+    if(pass == 12345) {console.log("Password Benar")}
+    else {console.log("Password Salah")}
+};
+
+Password(12345)
